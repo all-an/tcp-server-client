c6ee9a67c944c92d44ec93f4f2ed3d32a68fdf18
all-an <all.ancapistan@gmail.com>
2021-12-06 16:34:20 -0300

    learning

--- tcp/serverclienttcp/src/serverclienttcp/Server.java
+++ tcp/serverclienttcp/src/serverclienttcp/Server.java
@@ -1,7 +1,10 @@
 package serverclienttcp;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server implements Runnable {
 
@@ -17,6 +20,14 @@ public class Server implements Runnable {
     }
 
     class ConnectionHandler implements Runnable{
+
+        private Socket client;
+        private BufferedReader in;
+        private PrintWriter out;
+
+        public ConnectionHandler(Socket client){
+            this.client = client;
+        }
 
         @Override
         public void run(){
