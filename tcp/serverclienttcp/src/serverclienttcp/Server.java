bbaa7dbf135e7dee66fe1c4dfc80b73f753c9442
all-an <all-an@hotmail.com>
2021-12-08 09:04:17 -0300

    handling

--- tcp/serverclienttcp/src/serverclienttcp/Server.java
+++ tcp/serverclienttcp/src/serverclienttcp/Server.java
@@ -2,17 +2,23 @@ package serverclienttcp;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Server implements Runnable {
+
+    private ArrayList<ConnectionHandler> connections;
 
     @Override
     public void run(){
         try {
             ServerSocket server = new ServerSocket(9999);
             Socket client = server.accept();
+            ConnectionHandler handler = new ConnectionHandler(client);
+            connections.add(handler);
         } catch (IOException e) {
             // TODO: handle
         }
@@ -24,6 +30,7 @@ public class Server implements Runnable {
         private Socket client;
         private BufferedReader in;
         private PrintWriter out;
+        private String nickname;
 
         public ConnectionHandler(Socket client){
             this.client = client;
@@ -31,7 +38,15 @@ public class Server implements Runnable {
 
         @Override
         public void run(){
-
+            try{
+                out = new PrintWriter(client.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                out.println("Please enter a nickname: ");
+                nickname = in.readLine();
+                System.out.println(nickname + " connected !");
+            }catch(IOException e){
+                //TODO: handle
+            }
         }
 
     }
